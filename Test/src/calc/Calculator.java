15529ed825d0941153a1bd473847ac13bb4285a1
dir-FY24kurokijuon <juon.kuroki.k710@dirbato.co.jp>
2024-04-12 10:00:33 +0900

    最初のコミット（lesson6.6)2024.4.12

---
+++ Test/src/calc/Calculator.java
@@ -0,0 +1,13 @@
+package calc;
+
+public class Calculator {
+   public int add(int a, int b) {
+       return a + b;
+   }
+   
+   public int subtract(int a, int b) {
+       return a - b;
+       
+   }
+}
+//足し算を行うadd（）メソッド、引き算を行うsubtract（）メソッドがある
